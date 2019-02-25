db4deb4ee10703355247b9b84f826ff607806daa
ver217 <lhx0217@gmail.com>
2019-02-25 16:08:56 +0800

    merged ccmb

--- cpu.srcs/sources_1/new/up_down_ctr.v
+++ cpu.srcs/sources_1/new/up_down_ctr.v
@@ -4,44 +4,19 @@ module up_down_ctr(
     input clk_native,
     input key_up,
     input key_down,
-    output reg[31:0] address = 0
+    output reg[31:0] address
     );
-
+    wire key_up_done, key_down_done;
+    anti_shake as1(clk_native, key_up, key_up_done);
+    anti_shake as2(clk_native, key_down, key_down_done);
     initial address = 0;
 
-//    reg[15:0] debounce_up_reg = 0;
-//    reg[15:0] debounce_down_reg = 0;
-
-    always @(
-        key_up,
-        key_down,
-//        debounce_up_reg,
-//        debounce_down_reg,
-        address,
-        clk_native
-    ) begin
-//        if (debounce_up_reg > 0) begin
-//            debounce_up_reg = debounce_up_reg - 1;
-//        end
-    
-//        if (debounce_down_reg > 0) begin
-//            debounce_down_reg = debounce_down_reg - 1;
-//        end
-        
-        if(
-            key_up/* &&
-            debounce_up_reg == 0*/
-        ) begin
+    always @(posedge clk_native) begin
+        if (key_down_done) begin
             address = address > 4091 ? address : address + 4;
-//            debounce_up_reg = 16'hFFFF;
         end
-
-        if(
-            key_down /*&&
-            debounce_down_reg == 0*/
-        ) begin
+        if(key_up_done) begin
             address = address > 3 ? address - 4 : address;
-//            debounce_down_reg = 16'hFFFF;
         end
     end
 endmodule
