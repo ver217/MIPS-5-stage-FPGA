0b9fdd932960f0b85fedfd91bf3e0ea13f941faf
ver217 <lhx0217@gmail.com>
2019-02-27 14:08:29 +0800

    feat: add btn

--- cpu.srcs/sources_1/new/top.v
+++ cpu.srcs/sources_1/new/top.v
@@ -5,12 +5,13 @@ module top(
 	input show_clock_count,
 	input show_unconditional_branch_count,
 	input show_conditional_branch_count,
+	input show_loaduse_count,
 	input show_mem,
 	input key_up,
 	input key_down,
 	input[4:0] level,
-	output reg[7:0] AN,
-    output reg[7:0] seg
+	output [7:0] AN,
+    output [7:0] seg
     );
 	
 	wire CLK;
@@ -296,67 +297,78 @@ module top(
     wire [7:0] mem_seg;
 
 	reg[2:0] select = 0;
-
-
-	always @(
-		show_clock_count,
-		show_unconditional_branch_count,
-		show_conditional_branch_count,
-		show_mem,
-		RST
-	) begin
-		casez ({
-			show_mem,
-			show_conditional_branch_count,
-			show_unconditional_branch_count,
-			show_clock_count
-		})
-			4'b1ZZZ: select = 3;
-			4'b01ZZ: select = 2;
-			4'b001Z: select = 1;
-			4'b0001: select = 0;
-			4'b0000: select = 4;
-			default: select = 3'b111;
-		endcase
-		
-		if (RST) select = 3'b111;
-	end
-
-	always @(
-	   select,
-	   counter_AN,
-	   counter_seg,
-	   mem_AN,
-	   mem_seg,
-	   pause_AN,
-	   pause_seg
-    ) begin
-		if (select == 0 || select == 1 || select == 2) begin
-			AN = counter_AN;
-			seg = ~counter_seg;
-		end else if (select == 3) begin
-			AN = mem_AN;
-			seg = mem_seg;
-		end else if (select == 4) begin
-			AN = pause_AN;
-			seg = pause_seg;
-		end else begin
-			AN = 8'hff;
-			seg = 8'hff;
-		end
-	end
-
-	Information_display info(
+	wire [31:0] LedData;	
+	reg32 LedData_reg(AluB, Disp, CLK, RST, LedData);
+	Display show_syscall_display(
+		.reset(RST),
 		.clk(clk_native),
-		.clk_N(CLK),
-      	.reset(RST),
-		.conditional_branch_counter_en(Branch & PC_EN),
-		.unconditional_branch_counter_en(EX_JMP & PC_EN),
-		.clock_counter_en(EN),
-		.select(select),
-		.display(counter_seg),
-		.AN(counter_AN)
+		.data(LedData),
+		.seg(pause_seg),
+		.AN(pause_AN)
 	);
+
+	assign seg = pause_seg;
+	assign AN = pause_AN;
+
+	// always @(
+	// 	show_clock_count,
+	// 	show_unconditional_branch_count,
+	// 	show_conditional_branch_count,
+	// 	show_mem,
+	// 	RST
+	// ) begin
+	// 	casez ({
+	// 		show_mem,
+	// 		show_conditional_branch_count,
+	// 		show_unconditional_branch_count,
+	// 		show_clock_count
+	// 	})
+	// 		4'b1ZZZ: select = 3;
+	// 		4'b01ZZ: select = 2;
+	// 		4'b001Z: select = 1;
+	// 		4'b0001: select = 0;
+	// 		4'b0000: select = 4;
+	// 		default: select = 3'b111;
+	// 	endcase
+		
+	// 	if (RST) select = 3'b111;
+	// end
+
+	// always @(
+	//    select,
+	//    counter_AN,
+	//    counter_seg,
+	//    mem_AN,
+	//    mem_seg,
+	//    pause_AN,
+	//    pause_seg
+    // ) begin
+	// 	if (select == 0 || select == 1 || select == 2) begin
+	// 		AN = counter_AN;
+	// 		seg = ~counter_seg;
+	// 	end else if (select == 3) begin
+	// 		AN = mem_AN;
+	// 		seg = mem_seg;
+	// 	end else if (select == 4) begin
+	// 		AN = pause_AN;
+	// 		seg = pause_seg;
+	// 	end else begin
+	// 		AN = 8'hff;
+	// 		seg = 8'hff;
+	// 	end
+	// end
+
+	// Information_display info(
+	// 	.clk(clk_native),
+	// 	.clk_N(CLK),
+    //   	.reset(RST),
+	// 	.conditional_branch_counter_en(Branch & PC_EN),
+	// 	.unconditional_branch_counter_en(EX_JMP & PC_EN),
+	// 	.clock_counter_en(EN),
+	// 	.select(select),
+	// 	.display(counter_seg),
+	// 	.AN(counter_AN)
+	// );
 	
 	up_down_ctr up_down_ctr(
 		.clk_native(clk_native),
@@ -371,16 +383,6 @@ module top(
 		.data(data_out_display),
 		.seg(mem_seg),
 		.AN(mem_AN)
-	);
-
-	wire [31:0] LedData;
-	reg32 LedData_reg(AluB, Disp, CLK, RST, LedData);
-	Display show_syscall_display(
-		.reset(RST),
-		.clk(clk_native),
-		.data(LedData),
-		.seg(pause_seg),
-		.AN(pause_AN)
 	);
 
 endmodule
